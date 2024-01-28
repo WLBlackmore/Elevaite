7c60bac913aeb59bb67e7f7dfe29e635f609565b
William Blackmore <william.blackmore@mail.mcgill.ca>
2024-01-27 22:46:45 -0500

    getting player stats works!

--- server/players.py
+++ server/players.py
@@ -1,13 +1,18 @@
+from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 
-# LeBron James's Player ID
-lebron_id = '2544'  # Replace with the actual Player ID
+def getPlayerIdFromName(player_name):
+    all_players = players.get_players()
+    player_info = [player for player in all_players if player['full_name'] == player_name][0]
+    player_id = player_info['id']
+    return player_id
 
-# Create an instance of the PlayerCareerStats endpoint
-career_stats = playercareerstats.PlayerCareerStats(player_id=lebron_id, per_mode36='PerGame')
+def getPlayerSeasonStats(player_name, year):
+    found_player_id = getPlayerIdFromName(player_name)
+    career_stats = playercareerstats.PlayerCareerStats(player_id=found_player_id, per_mode36='PerGame')
+    career_df = career_stats.get_data_frames()[0]
+    player_szn_stats = career_df[career_df['SEASON_ID'] == year]
+    print(player_name + " has these stats in year " + year + '\n')
+    print(player_szn_stats)
 
-# Extract the data for the 2022 season
-career_df = career_stats.get_data_frames()[0]
-season_2022_stats = career_df[career_df['SEASON_ID'] == '2021-22']
-
-print(season_2022_stats.PTS)
+getPlayerSeasonStats("LeBron James", "2021-22")
